b0466584b273e5c8a81ff4f37df3eeeab9bb21b9
Kefas Kingsley <kefasiceking@gmail.com>
2024-12-18 07:19:08 +0100

    bug: fix type inferring errors

--- frontend-react/src/components/EventList.tsx
+++ frontend-react/src/components/EventList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card';
 import { Button } from './ui/button';
